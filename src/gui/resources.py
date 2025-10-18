5ca1ec9c28cdb9790c42750438a923aa562a02b6
Granfalegion <141968642+Granfalegion@users.noreply.github.com>
2025-10-18 01:16:03 -0400

    Some light productionization

--- src/gui/resources.py
+++ src/gui/resources.py
@@ -1,9 +1,12 @@
 import os, sys
 
-def getPath(relativePath):
+def getPath(*relativePathSteps):
     """Creates and returns absolute path to resource. Works both in a
     PyInstaller-created executable and in a development environment.
     """
+
+    relativePath = os.path.join(*relativePathSteps)
+
     if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
         # Attempt to load resources from PyInstaller's temp folder, if present.
         base_path = getattr(sys, '_MEIPASS')
